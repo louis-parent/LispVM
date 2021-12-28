083f4f86e668d92a1b78a66ddceccce031e87578
Louis Parent <contact@louis-parent.fr>
2021-12-28 14:48:19 +0100

    Adding some comments

--- src/compiler/main.lisp
+++ src/compiler/main.lisp
@@ -7,10 +7,14 @@
 
 (defun compile_lisp_expression (expression)
 	(if (atom expression)
+		
+		; Cas d'un littéral ou d'un symbole
 		(if (numberp expression)
 			(compile_lit_expression expression)
-			(cons expression '())
+			(cons expression '()) ; TODO : Symbole de variable à interpréter => ajout de l'environement
 		)
+		
+		; Cas d'une expression à évaluer
 		(let (
 				(operator (car expression))
 				(args (cdr expression))
@@ -19,6 +23,7 @@
 				((eq operator '+)
 					(compile_add_expression args)
 				)
+				; TODO Switch sur tout les opérateurs de base
 			)
 		)
 	)
@@ -31,20 +36,20 @@
 		)
 		(append
 			(append 
-				(append (compile_lisp_expression left) '((PUSH R0)))
-				(append (compile_lisp_expression right) '((PUSH R0)))
+				(append (compile_lisp_expression left) '((PUSH R0))) ; Sauvegarde de l'opérante de gauche dans la pile
+				(append (compile_lisp_expression right) '((PUSH R0))); Sauvegarde de l'opérante de droite dans la pile
 			)
 			'(
-				(POP R0)
-				(POP R1)
-				(ADD R1 R0)
+				(POP R0) ; Récupération de l'opérante de droite
+				(POP R1) ; Récupération de l'opérante de gauche
+				(ADD R1 R0) ; Calcul du résultat et stockage dans R0
 			)
 		)
 	)
 )
 
 (defun compile_lit_expression (value)
-	(cons (list 'MOVE (list ':CONST value) 'R0) NIL)
+	(cons (list 'MOVE (list ':CONST value) 'R0) NIL) ; Stockage de la valeur dans R0
 )
 
 (compile_lisp '((+ 1 1)))
