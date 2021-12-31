8490f31abba5efa46edc7801bfebf69f66441049
Louis Parent <contact@louis-parent.fr>
2021-12-31 12:01:53 +0100

    Adding let compilation

--- src/compiler/main.lisp
+++ src/compiler/main.lisp
@@ -5,6 +5,7 @@
 (defun compile_lisp (expressions)
 	(setf (get '__LISP_COMPILER__ '__GLOBALS__) '())
 	(setf (get '__LISP_COMPILER__ '__LABELS__) '())
+	(setf (get '__LISP_COMPILER__ '__FRAME_OFFSET_) '())
 	(compile_lisp_expressions expressions '())
 )
 
@@ -50,28 +51,31 @@
 				)
 				((eql operator '=)
                     (compile_equal_expression args environment)
-                )
-                ((eql operator '/=)
-                    (compile_not_equal_expression args environment)
-                )
-                ((eql operator '>)
-                    (compile_greater_expression args environment)
-                )
-                ((eql operator '>=)
-                    (compile_greater_equal_expression args environment)
-                )
-                ((eql operator '<)
-                    (compile_lower_expression args environment)
-                )
-                ((eql operator '<=)
-                    (compile_lower_equal_expression args environment)
-                )
-                ((eql operator 'null)
-                    (compile_null_expression args environment)
-                )
-                ((eql operator 'not)
-                    (compile_not_expression args environment)
-                )
+        )
+        ((eql operator '/=)
+            (compile_not_equal_expression args environment)
+        )
+        ((eql operator '>)
+            (compile_greater_expression args environment)
+        )
+        ((eql operator '>=)
+            (compile_greater_equal_expression args environment)
+        )
+        ((eql operator '<)
+            (compile_lower_expression args environment)
+        )
+        ((eql operator '<=)
+            (compile_lower_equal_expression args environment)
+        )
+        ((eql operator 'null)
+            (compile_null_expression args environment)
+        )
+        ((eql operator 'not)
+            (compile_not_expression args environment)
+        )
+        ((eql operator 'let)
+          (compile_let_expression args environment)
+        )
 				; TODO Switch sur tout les opérateurs de base
 			)
 		)
@@ -107,13 +111,13 @@
 		)
 		(append
 			(append 
-				(append (compile_lisp_expression left environment) '((PUSH R0))) ; Sauvegarde de l'opérante de gauche dans la pile
-				(append (compile_lisp_expression right environment) '((PUSH R0))); Sauvegarde de l'opérante de droite dans la pile
+				(append (compile_lisp_expression left environment) (list (create_push_instruction 'R0))) ; Sauvegarde de l'opérante de gauche dans la pile
+				(append (compile_lisp_expression right environment) (list (create_push_instruction 'R0))); Sauvegarde de l'opérante de droite dans la pile
 			)
-			'(
-				(POP R0) ; Récupération de l'opérante de droite
-				(POP R1) ; Récupération de l'opérante de gauche
-				(ADD R1 R0) ; Calcul du résultat et stockage dans R0
+			(list
+				(create_pop_instruction 'R0) ; Récupération de l'opérante de droite
+				(create_pop_instruction 'R1) ; Récupération de l'opérante de gauche
+				'(ADD R1 R0) ; Calcul du résultat et stockage dans R0
 			)
 		)
 	)
@@ -126,13 +130,13 @@
 		)
 		(append
 			(append 
-				(append (compile_lisp_expression left environment) '((PUSH R0))) ; Sauvegarde de l'opérante de gauche dans la pile
-				(append (compile_lisp_expression right environment) '((PUSH R0))); Sauvegarde de l'opérante de droite dans la pile
+				(append (compile_lisp_expression left environment) (list (create_push_instruction 'R0))) ; Sauvegarde de l'opérante de gauche dans la pile
+				(append (compile_lisp_expression right environment) (list (create_push_instruction 'R0))); Sauvegarde de l'opérante de droite dans la pile
 			)
-			'(
-				(POP R0) ; Récupération de l'opérante de droite
-				(POP R1) ; Récupération de l'opérante de gauche
-				(SUB R1 R0) ; Calcul du résultat et stockage dans R0
+			(list
+				(create_pop_instruction 'R0) ; Récupération de l'opérante de droite
+				(create_pop_instruction 'R1) ; Récupération de l'opérante de gauche
+				'(SUB R1 R0) ; Calcul du résultat et stockage dans R0
 			)
 		)
 	)
@@ -145,13 +149,13 @@
 		)
 		(append
 			(append 
-				(append (compile_lisp_expression left environment) '((PUSH R0))) ; Sauvegarde de l'opérante de gauche dans la pile
-				(append (compile_lisp_expression right environment) '((PUSH R0))); Sauvegarde de l'opérante de droite dans la pile
+				(append (compile_lisp_expression left environment) (list (create_push_instruction 'R0))) ; Sauvegarde de l'opérante de gauche dans la pile
+				(append (compile_lisp_expression right environment) (list (create_push_instruction 'R0))); Sauvegarde de l'opérante de droite dans la pile
 			)
-			'(
-				(POP R0) ; Récupération de l'opérante de droite
-				(POP R1) ; Récupération de l'opérante de gauche
-				(MUL R1 R0) ; Calcul du résultat et stockage dans R0
+			(list
+				(create_pop_instruction 'R0) ; Récupération de l'opérante de droite
+				(create_pop_instruction 'R1) ; Récupération de l'opérante de gauche
+				'(MUL R1 R0) ; Calcul du résultat et stockage dans R0
 			)
 		)
 	)
@@ -164,13 +168,13 @@
 		)
 		(append
 			(append 
-				(append (compile_lisp_expression left environment) '((PUSH R0))) ; Sauvegarde de l'opérante de gauche dans la pile
-				(append (compile_lisp_expression right environment) '((PUSH R0))); Sauvegarde de l'opérante de droite dans la pile
+				(append (compile_lisp_expression left environment) (list (create_push_instruction 'R0))) ; Sauvegarde de l'opérante de gauche dans la pile
+				(append (compile_lisp_expression right environment) (list (create_push_instruction 'R0))); Sauvegarde de l'opérante de droite dans la pile
 			)
-			'(
-				(POP R0) ; Récupération de l'opérante de droite
-				(POP R1) ; Récupération de l'opérante de gauche
-				(DIV R1 R0) ; Calcul du résultat et stockage dans R0
+			(list
+				(create_pop_instruction 'R0) ; Récupération de l'opérante de droite
+				(create_pop_instruction 'R1) ; Récupération de l'opérante de gauche
+				'(DIV R1 R0) ; Calcul du résultat et stockage dans R0
 			)
 		)
 	)
@@ -184,18 +188,25 @@
             (label_skip (create_label "skip_function"))
 		)
 		(add_function_to_globals name (create_function_params_offset_list params))
-		(append 
-			(list
-				(list 'JMP label_skip)
-				(list 'LABEL name)
-			)
-			(append
-				(compile_lisp_expressions body (append environment (create_function_params_offset_list params)))
-				(list 
-					(list 'RETURN)
-					(list 'LABEL label_skip)
+		(push_new_frame_context)
+		(let (
+				(bytecode (append 
+							(list
+								(list 'JMP label_skip)
+								(list 'LABEL name)
+							)
+							(append
+								(compile_lisp_expressions body (append environment (create_function_params_offset_list params)))
+								(list 
+									(list 'RETURN)
+									(list 'LABEL label_skip)
+								)
+							)
+						)				
 				)
 			)
+			(pop_new_frame_context)
+			bytecode
 		)
 	)
 )
@@ -260,9 +271,9 @@
         
         (append
             (append arg1
-            (append (list (list 'PUSH 'R0))
+            (append (list (create_push_instruction 'R0))
             (append arg2)
-            (append (list (list 'POP 'R1))
+            (append (list (create_pop_instruction 'R1))
             (append (list (list 'CMP 'R1 'R0))
             (append (list (list 'JEQ label_true))
             (append (list (list 'MOVE '(:CONST NIL) 'R0))
@@ -285,9 +296,9 @@
         
         (append
             (append arg1
-            (append (list (list 'PUSH 'R0))
+            (append (list (create_push_instruction 'R0))
             (append arg2)
-            (append (list (list 'POP 'R1))
+            (append (list (create_pop_instruction 'R1))
             (append (list (list 'CMP 'R1 'R0))
             (append (list (list 'JNE label_true))
             (append (list (list 'MOVE '(:CONST NIL) 'R0))
@@ -310,9 +321,9 @@
         
         (append
             (append arg1
-            (append (list (list 'PUSH 'R0))
+            (append (list (create_push_instruction 'R0))
             (append arg2)
-            (append (list (list 'POP 'R1))
+            (append (list (create_pop_instruction 'R1))
             (append (list (list 'CMP 'R1 'R0))
             (append (list (list 'JGT label_true))
             (append (list (list 'MOVE '(:CONST NIL) 'R0))
@@ -335,9 +346,9 @@
         
         (append
             (append arg1
-            (append (list (list 'PUSH 'R0))
+            (append (list (create_push_instruction 'R0))
             (append arg2)
-            (append (list (list 'POP 'R1))
+            (append (list (create_pop_instruction 'R1))
             (append (list (list 'CMP 'R1 'R0))
             (append (list (list 'JGT label_true))
             (append (list (list 'MOVE '(:CONST NIL) 'R0))
@@ -360,9 +371,9 @@
         
         (append
             (append arg1
-            (append (list (list 'PUSH 'R0))
+            (append (list (create_push_instruction 'R0))
             (append arg2)
-            (append (list (list 'POP 'R1))
+            (append (list (create_pop_instruction 'R1))
             (append (list (list 'CMP 'R1 'R0))
             (append (list (list 'JLT label_true))
             (append (list (list 'MOVE '(:CONST NIL) 'R0))
@@ -385,9 +396,9 @@
         
         (append
             (append arg1
-            (append (list (list 'PUSH 'R0))
+            (append (list (create_push_instruction 'R0))
             (append arg2)
-            (append (list (list 'POP 'R1))
+            (append (list (create_pop_instruction 'R1))
             (append (list (list 'CMP 'R1 'R0))
             (append (list (list 'JLE label_true))
             (append (list (list 'MOVE '(:CONST NIL) 'R0))
@@ -469,16 +480,120 @@
     )
 )
 
-;(write (compile_lisp '(
-;    (not (+ 4 5))
-;)))
+(defun compile_let_expression (args environment)
+	(let (
+			(vars (car args))
+			(expressions (cdr args))
+			(local_environment (create_environment_for_locals (car args)))
+		)
+		(append
+			(create_push_for_locals vars environment)
+			(append
+				(compile_lisp_expressions expressions (append environment local_environment))	
+				(create_pop_for_locals vars)
+			)
+		)
+	)
+)
+
+(defun create_push_for_locals (vars environment)
+	(if (null vars)
+		NIL
+		(append
+			(compile_lisp_expression (cadar vars) environment)
+			(append 
+				(list '(PUSH R0))
+				(create_push_for_locals (cdr vars) environment)
+			)
+		)
+	)
+)
+
+(defun create_pop_for_locals (vars)
+	(if (null vars)
+		NIL
+		(append 
+			(list (create_pop_instruction 'R2))
+			(create_pop_for_locals (cdr vars))
+		)
+	)
+)
+
+(defun create_environment_for_locals (vars)
+	(if (null vars)
+		NIL
+		(cons 
+			(list
+				(caar vars) 
+				(progn 
+					(increment_frame_offset)
+					(car (last (get '__LISP_COMPILER__ '__FRAME_OFFSET_)))
+				)
+			)
+			(create_environment_for_locals (cdr vars))
+		)
+	)
+)
+
+(defun push_new_frame_context ()
+	(setf (get '__LISP_COMPILER__ '__FRAME_OFFSET_) (append (get '__LISP_COMPILER__ '__FRAME_OFFSET_) '(3)))
+)
+
+(defun pop_new_frame_context ()
+	(setf (get '__LISP_COMPILER__ '__FRAME_OFFSET_) (without_last (get '__LISP_COMPILER__ '__FRAME_OFFSET_)))
+)
+
+(defun increment_frame_offset ()
+	(setf (get '__LISP_COMPILER__ '__FRAME_OFFSET_) (increment_last (get '__LISP_COMPILER__ '__FRAME_OFFSET_)))
+)
+
+(defun decrement_frame_offset ()
+	(setf (get '__LISP_COMPILER__ '__FRAME_OFFSET_) (decrement_last (get '__LISP_COMPILER__ '__FRAME_OFFSET_)))
+)
+
+(defun increment_last (l)
+	(if (null l)
+		NIL
+		(if (null (cdr l))
+			(cons (+ (car l) 1) NIL)
+			(cons (car l) (increment_last (cdr l)))
+		)
+	)
+)
+
+(defun decrement_last (l)
+	(if (null l)
+		NIL
+		(if (null (cdr l))
+			(cons (- (car l) 1) NIL)
+			(cons (car l) (decrement_last (cdr l)))
+		)
+	)
+)
+
+(defun without_last (l)
+	(reverse (cdr (reverse l)))
+)
+
+(defun create_push_instruction (register)
+	(increment_frame_offset)
+	(list 'PUSH register)
+)
+
+(defun create_pop_instruction (register)
+	(decrement_frame_offset)
+	(list 'POP register)
+)
 
 (write (compile_lisp '(
-    (if (> (+ 11 2) (- 6 1))
-        (if  (not (null (/= 3 4)))
-            1
-            0
-        )
-        (+ 2 1)
+    (defun f (x)
+    	(let (
+    			(a (+ x 5))
+    			(b 2)
+    		)
+    		(* a b)
+    	)
     )
+    
+    (f 1)
 )))
